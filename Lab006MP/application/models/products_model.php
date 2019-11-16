2553ceac95325cf6c41a88ff277210de92e28d73
Lorenz Ras <lenzras@gmail.com>
2019-11-16 21:13:34 +0900

    Update products_model.php

--- Lab006MP/application/models/products_model.php
+++ Lab006MP/application/models/products_model.php
@@ -9,22 +9,7 @@ class Products_model extends CI_Model{
 		parent::__construct();
 		$this->load->database();
 	}
-	
-	//Method to get data from the products table.
-	//It accepts one paramater to specify the row to be returned.
-	public function get_products($foodName = null){		
-		if($foodName==null){ 
-			//If parameter is empty, return all rows.
-			$rs = $this->db->get('products');
-			return $rs->result_array();
-		}else{
-			//otherwise, return  specific row based parameter.
-			//This query can also handle data with spaces.
-			$rs = $this->db->get_where('products', array('name' => urldecode($foodName)));
-			return $rs->row_array();
-		}
-	}
-	
+		
 	public function getProducts($id=null){
 		if($id==null){
 			$rs = $this->db->get('products');
